f04a38be4e6a9d2043dc308694018faa4fab067a
Patrick Shobe <patrick@patrickshobe.com>
2018-09-03 12:03:25 -0600

    Closes #16

--- app/controllers/admin/user_activities_controller.rb
+++ app/controllers/admin/user_activities_controller.rb
@@ -2,4 +2,8 @@ class Admin::UserActivitiesController < Admin::BaseController
   def new
     @user_activity = UserActivity.new
   end
+
+  def index
+    @activities = UserActivity.all
+  end
 end
--- spec/features/user_can_delete_spec.rb
+++ spec/features/user_can_delete_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe' User Interactions' do
+describe 'User Interactions' do
   before(:each) do
     @user = create(:user)
     visit login_path
@@ -16,6 +16,30 @@ describe' User Interactions' do
 
     expect(page).to have_content(activity.title)
 
+    click_on 'Delete'
 
+    expect(page).to_not have_content(activity.title)
+  end
+end
+describe 'Admin Interactions' do
+  before(:each) do
+    @admin = create(:user, role: 1)
+    visit login_path
+
+    fill_in 'username', with: @admin.username
+    fill_in 'password', with: 'test'
+    click_on 'Log In'
+  end
+  it 'should allow users to delete their user_activities' do
+    activity = create(:activity)
+    user = create(:user, username: 'Todd')
+    user.user_activities.create(activity_id: activity.id)
+    visit admin_user_activities_path
+
+    expect(page).to have_content(activity.title)
+
+    click_on 'Delete'
+
+    expect(page).to_not have_content(activity.title)
   end
 end
