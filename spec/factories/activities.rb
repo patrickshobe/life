05fe9aacaa179556385d47eaba159df323e25523
Patrick Shobe <patrick@patrickshobe.com>
2018-09-02 19:20:53 -0600

    Closes #8

---
+++ spec/factories/activities.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :activity do
+    title {'Write Tests'}
+    points  {'100'}
+    category {'Coding'}
+  end
+end
+
--- spec/features/activities/users_can_see_all_activities_spec.rb
+++ spec/features/activities/users_can_see_all_activities_spec.rb
@@ -10,10 +10,13 @@ describe 'User Interactions' do
     click_on 'Log In'
   end
   it 'should show the user all of the activities' do
-
+    activity_1 = create(:activity)
+    activity_2 = create(:activity, title: 'Clean Your Room')
 
     visit activities_path
 
     expect(page).to have_content('All Activities')
+    expect(page).to have_content(activity_1.title)
+    expect(page).to have_content(activity_2.title)
   end
 end
