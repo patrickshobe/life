06803f2b8d8e254b9a5d0b7dbf78b511cc0a3821
Patrick Shobe <patrick@patrickshobe.com>
2018-09-02 21:01:58 -0600

    Closes #10

--- spec/features/activities/users_can_see_all_activities_spec.rb
+++ spec/features/activities/users_can_see_all_activities_spec.rb
@@ -20,3 +20,50 @@ describe 'User Interactions' do
     expect(page).to have_content(activity_2.title)
   end
 end
+describe 'Admin Interactions' do
+  before(:each) do
+    admin = create(:user, role: 1)
+    visit login_path
+
+    fill_in 'username', with: admin.username
+    fill_in 'password', with: 'test'
+    click_on 'Log In'
+  end
+  it 'should show all of the activities' do
+    activity_1 = create(:activity)
+    activity_2 = create(:activity, title: 'Play Shuffleboard')
+
+    visit admin_activities_path
+
+    expect(page).to have_content(activity_1.title)
+    expect(page).to have_content(activity_2.title)
+  end
+  it 'should have an edit button' do
+    activity = create(:activity)
+
+    visit admin_activities_path
+
+    expect(page).to have_content(activity.title)
+
+    expect(page).to have_link('Edit')
+
+    click_on 'Edit'
+
+    expect(current_path).to eq(edit_admin_activity_path(activity))
+  end
+  it 'should have a delete button' do
+    activity = create(:activity)
+
+    visit admin_activities_path
+
+    expect(page).to have_content(activity.title)
+
+    expect(page).to have_link('Delete')
+
+    click_on 'Delete'
+
+    expect(current_path).to eq(admin_activities_path)
+    expect(page).to have_content("Activity #{activity.title} Deleted")
+  end
+
+end
